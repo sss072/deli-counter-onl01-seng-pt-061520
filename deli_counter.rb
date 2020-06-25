c7511d62ef300f1340a61f4f6dc570649932ee4b
Sami  Sabeeh <samsabeeh6@gmail.com>
2020-06-25 00:11:46 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,14 @@
-# Write your code here.
+
+def line(array)
+  if array.length == 0 
+    puts "The line is currently empty."
+  else
+    counter = 1 
+    array.each do |element|
+      puts "The line is currently: #{counter}. #{element}"
+      counter += 1 
+    end
+  end
+end
+def take_a_number(katz_deli, line)
+end
